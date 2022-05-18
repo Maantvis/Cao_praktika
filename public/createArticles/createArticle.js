0fbe53a8cf7b1741fb598cb951abc1c45b7aa48a
Mantas <m.juozenas@gmail.com>
2022-05-18 22:52:08 +0300

    responsive design done

--- public/createArticles/createArticle.js
+++ public/createArticles/createArticle.js
@@ -46,9 +46,11 @@ formEl.addEventListener('submit', (e) => {
 
   postArticles(token);
 });
-const signOutEl = document.getElementById('signOut');
-signOutEl.addEventListener('click', () => {
-  localStorage.removeItem('userToken');
-  localStorage.removeItem('userId');
-  window.location.replace('../login/login.html');
+const signOutEl = document.querySelectorAll('.signOut');
+signOutEl.forEach((signOut) => {
+  signOut.addEventListener('click', () => {
+    localStorage.removeItem('userToken');
+    localStorage.removeItem('userId');
+    window.location.replace('../login/login.html');
+  });
 });
