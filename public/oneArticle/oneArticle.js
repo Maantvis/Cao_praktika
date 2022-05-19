d80fb60d0eba15d71fc9f6a9d700926da8fd256c
Mantvydas <mantvydas.dirgincius@icloud.comm>
2022-05-19 14:48:44 +0300

    form show

--- public/oneArticle/oneArticle.js
+++ public/oneArticle/oneArticle.js
@@ -6,6 +6,14 @@ const id = location.search.slice(2);
 const titleEl = document.getElementById('title');
 const contentEl = document.getElementById('content');
 const dateEl = document.getElementById('date');
+const editBtn = document.getElementById('editBtn');
+
+const titleInpEl = document.getElementById('titleInp');
+const contentInpEl = document.getElementById('contentInp');
+const dateInpEl = document.getElementById('dateInp');
+const submitBtnEl = document.getElementById('submitBtn');
+
+let article;
 if (!token) {
   window.location.replace('../login/login.html');
 }
@@ -30,6 +38,32 @@ async function getArticles(token) {
   const data = await resp.json();
   console.log('data ===', data);
   renderContent(data.article);
+  article = data.article[0].user_id;
 }
 
 getArticles(token);
+
+editBtn.addEventListener('click', () => {
+  if (article !== +localStorage.getItem('userId')) {
+    alert('You can only edit your post');
+    return;
+  }
+
+  editBtn.textContent === 'Edit'
+    ? (editBtn.textContent = 'Cancel')
+    : (editBtn.textContent = 'Edit');
+
+  titleInpEl.classList.toggle('hidden');
+  contentInpEl.classList.toggle('hidden');
+  dateInpEl.classList.toggle('hidden');
+  submitBtnEl.classList.toggle('hidden');
+});
+submitBtnEl.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const obj = {
+    date: dateInpEl.value,
+    title: titleInpEl.value,
+    content: contentInpEl.value,
+  };
+});
